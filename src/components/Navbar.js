c8bf92e3e9c6d237be074915599cee0c31dbb440
subhamexplore <subhamparidamain@gmail.com>
2025-01-04 14:10:44 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../assets/styles/Navbar.css";
 import logo from "../assets/images/logoNew.png";
+import logoCol from "../assets/images/flogo.png";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { Link, useNavigate } from "react-router-dom";
 import Contact from "../pages/Contact";
@@ -8,10 +9,175 @@ import Contact from "../pages/Contact";
 const Navbar = () => {
   const nav = useNavigate();
   const [cont, setCont] = useState(false);
+  const [click, setClick] = useState(false);
 
   return (
     <section id="nav-tdg" className="navbar-section">
-      {cont?<Contact setCont={setCont}/>:""}
+      {cont ? <Contact setCont={setCont} /> : ""}
+      {click ? (
+        <div className="navbar-col">
+          <div className="nav-top-col d-flex align-items-center my-3">
+            <img
+              src="https://i0.wp.com/www.thediallogroupllc.com/wp-content/uploads/2024/11/TDG-logo-dark.png?w=924&ssl=1"
+              height={130}
+              style={{
+                marginRight: "-33px",
+                paddingTop: "5px",
+              }}
+              alt=""
+            />
+            <div
+              className="industries"
+              style={{
+                marginLeft: "30px",
+                fontSize: "1.3rem",
+                color: "white",
+                fontWeight: "600",
+                width: "210px",
+                lineHeight: "23px",
+                borderLeft: "1px solid white",
+                height: "100px",
+                paddingLeft: "20px",
+                paddingTop: "25px",
+              }}
+            >
+              Efficiency Through Simplicity
+            </div>
+          </div>
+          <hr style={{ color: "grey" }} />
+          <nav className="nav-main navbar navbar-expand-lg">
+            <div
+              className={`collapse navbar-collapse ${click ? "show" : ""}`}
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mx-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item dropdown nav-drop-w">
+                  <div
+                    class="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    About Us
+                  </div>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a class="dropdown-item" href="/who-we-are">
+                        Who We Are
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/leadership">
+                        Leadership
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/why-us">
+                        Why Us
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/join-tdg">
+                        Join TDG
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown nav-drop-w">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Industries
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a class="dropdown-item" href="/public-sector">
+                        Public Sector
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/commercial">
+                        Commercial
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/health">
+                        Health
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown nav-drop-w">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Capabilities
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        href="/program-project-management"
+                      >
+                        Program & Project Management
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        href="/information-technology-cloud-solutions"
+                      >
+                        Information Technology and Cloud Solutions
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/download-capabilities">
+                        Download Capability Statement
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/contract-vehicles">
+                    Contract Vehicles
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/scoop">
+                    Scoop
+                  </a>
+                </li>
+              </ul>
+              <button
+                className="nav-button btn"
+                type="submit"
+                onClick={() => setCont(true)}
+              >
+                Contact Us
+              </button>
+            </div>
+          </nav>
+          <button className="cross-nav" onClick={() => setClick(false)}>
+            x
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
       <nav className="nav-main navbar navbar-expand-lg">
         <div className="container-fluid">
           <Link className="navbar-brand" to={"/"}>
@@ -20,17 +186,21 @@ const Navbar = () => {
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
+            // data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={() => setClick(!click)}
           >
             <span className="" style={{ color: "grey", fontSize: "30px" }}>
               <RxHamburgerMenu />
             </span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div
+            className={`collapse navbar-collapse ${click ? "show" : ""}`}
+            id="navbarSupportedContent"
+          >
             <ul className="navbar-nav mx-auto">
               <li className="nav-item">
                 <Link className="nav-link" to={"/"}>
